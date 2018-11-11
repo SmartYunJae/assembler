b722c62d39a91bbe3feb3ac48969f9a5392b0159
SmartYunJae <eff33@hanmail.net>
2018-11-11 19:15:47 +0900

    test

--- check.c
+++ check.c
@@ -5,7 +5,7 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	/********************************/
+	test/********************************/
     /*  							*/
 	/*         syntax check         */
     /*  							*/
