c4b6c5dbf7da57f7179cb7fd4c9ce2b273ebcd19
SmartYunJae <eff33@hanmail.net>
2018-11-12 21:26:06 +0900

    modify check.c & add Makefile

--- check.c
+++ check.c
@@ -10,11 +10,11 @@ int is_valid(char *op, char *args)
 	source = strtok(args,","); //divide with ',' 
 	destination = strtok(NULL, "\n");
 
-	if(source[0]='('&&destination[0]='(') //if source, destination is memory
+	if(source[0]=='('&&destination[0]=='(') //if source, destination is memory
 		return 0;
-	else if(source[0]='$'&&destination[0]='$') //if source, destination is immediate
+	else if(source[0]=='$'&&destination[0]=='$') //if source, destination is immediate
 		return 0;
-	else if(destination[0]='$') // if destination is immediate
+	else if(destination[0]=='$') // if destination is immediate
 		return 0;
 	else
 		return 1;
