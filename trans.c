958b3e06e400a770405cd46efaf56c4317117afe
SmartYunJae <eff33@hanmail.net>
2018-11-13 00:36:07 +0900

    modify trans.c

--- trans.c
+++ trans.c
@@ -4,27 +4,38 @@ int instr_trans(char *op, char *args, char* mcode)
 {
 	
 	char *source, *destination;
-	
+	/*
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
-	}
-
+	}*/
 	source = strtok(args,",");
 	destination = strtok(NULL,"\n");
 
 	if(source[0]=='%'&&destination[0]=='%') // reg to reg
 		strcpy(mcode,"89");
+
 	if(source[0]=='('&&destination[0]=='%') // mem to reg( addressing w.o/ disp)
 		strcpy(mcode,"8b");
+
 	if(source[0]=='-'&&destination[0]=='%') // mem to reg( addressing w/ disp)
 		strcpy(mcode,"8b"); 
+
 	if(source[0]=='0'&&destination[0]=='%') // mem to reg(eax)
 		strcpy(mcode,"a1");
+
 	if(source[0]=='%'&&destination[0]=='0') // reg(eax) to mem
 		strcpy(mcode,"a3");
-	if(source[0]=='$'&&destination[0]=='%') // immediate to reg
-		strcpy(mcode,"b8");
+
+	if(source[0]=='$'&&destination[0]=='%'){// immediate to reg
+		if(destination[2]=='c')
+			strcpy(mcode,"b9");
+		else if(destination[2]=='d')
+			strcpy(mcode,"ba");
+		else
+			strcpy(mcode,"b8");
+	}
+	
 
 
 	return 1;	
